50a26b514f202690bba0b16b9a54c2cf2bcbc15a
Ritesh Nagrikar <riteshnagrikar77@gmail.com>
2024-12-20 21:01:34 +0530

    edit navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.scss';
-// react router dom
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-// pages
 import { Home, MealDetails, Error, Category } from "./pages/index";
-// components
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 
